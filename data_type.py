41ee828ef112f34185a6f847c418e8a5fdbef5d6
Omar Faruk <126099945+Omarfarukhimel@users.noreply.github.com>
2023-03-29 12:06:14 +0600

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,15 @@
+name="omar faruk"
+id=1234
+cgpa=3.50
+x=False
+print("my name is "+name)
+print("the \"name\" of data type is ",type(name))
+print("length of my name",len(name))
+
+print("my student id is ",id)
+print("data type of \"id\" is ", type(id))
+
+print("CGPA = ",cgpa)
+print("data type of \"cgpa\"is",type(cgpa))
+
+print("data type of \'False\' is ",type(False))
---
+++ first_print.py
@@ -0,0 +1,6 @@
+#for single line print
+print("HELLO world\n")
+
+#for multiline print
+print("""my name is omar faruk himel
+      this is my first code""")
