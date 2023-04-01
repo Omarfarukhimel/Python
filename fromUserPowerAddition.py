4f50a71fbea86e74634afd4e2c8f918093ca2dc4
Omar Faruk <126099945+Omarfarukhimel@users.noreply.github.com>
2023-04-01 14:28:21 +0600

    Add files via upload

---
+++ fromUserPowerAddition.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a:-"))
+b=int(input("enter the value of b:-"))
+c=int(input ("Enter the value of c:-"))
+d=int(input("enter the value of d:-"))
+
+result=(a**b)+(c**d)
+print("the result of a^b + c^d is:-",result)
---
+++ input_function.py
@@ -0,0 +1,10 @@
+a=int(input("enter 1st number:-"))
+b=int(input("enter 2nd number:-"))
+x=a+b
+y=a-b
+z=a*b
+w=a/b
+print(x)
+print(y)
+print(z)
+print(w)
